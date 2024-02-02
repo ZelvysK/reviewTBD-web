4de6a2f18effbe793145318586ec68b2c53e8016
Karolis Zelvys <koralbish@gmail.com>
2024-02-02 19:40:30 +0200

    Changed add-studio method

--- src/components/studio/add-studio.tsx
+++ src/components/studio/add-studio.tsx
@@ -1,31 +1,41 @@
 import useAxios from "axios-hooks";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Select, { SingleValue } from "react-select";
-import {
-  NewStudio,
-  Option,
-  StudioOptions,
-  StudioType,
-  StudioTypes,
-} from "../../types";
+import { useNavigate, useParams } from "react-router-dom";
+import { StudioTypes } from "../../types";
 import { getUrl } from "../../utils/navigation";
+import { z } from "zod";
+import { Controller, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../form/select";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type CreateStudioResponse = {
-  id: string;
-  message: string;
-};
+const schema = z.object({
+  type: z.enum(StudioTypes),
+  name: z.string().min(3),
+  description: z.string().min(4),
+  imageUrl: z.string().url(),
+  dateCreated: z.string(),
+});
+
+type CreateStudioForm = z.infer<typeof schema>;
+
+// type CreateStudioResponse = {
+//   id: string;
+//   message: string;
+// };
 
 export const AddStudio = () => {
   const navigate = useNavigate();
-  const [studioType, setStudioType] =
-    useState<SingleValue<Option<StudioType>>>();
-  const [name, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
-  const [dateCreated, setDateCreated] = useState("");
+  const { studioId } = useParams();
 
-  const [{ loading, error }, executePost] = useAxios<CreateStudioResponse>(
+  const [_, executePost] = useAxios<CreateStudioForm>(
     {
       url: getUrl("studio"),
       method: "post",
@@ -33,105 +43,128 @@ export const AddStudio = () => {
     { manual: true }
   );
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const onSubmit = async (data: CreateStudioForm) => {
+    try {
+      const response = await executePost({ data: { id: studioId, ...data } });
 
-    const type = studioType?.value;
-
-    if (!type) {
-      return;
-    }
-
-    if (!StudioTypes.includes(type)) {
-      throw new Error("Invalid studio type");
-    }
-
-    const postData: NewStudio = {
-      name,
-      description,
-      imageUrl,
-      dateCreated,
-      type,
-    };
-
-    const { data } = await executePost({ data: postData });
-
-    const { id } = data;
-
-    navigate(`../../studio/${id}`);
-
-    if (error) {
-      throw new Error(error.message);
+      if (response.status === 200) {
+        navigate(`../../studio/${studioId}`);
+        toast.success("Studio created successfully");
+      }
+    } catch (error) {
+      toast.error("Failed");
     }
   };
 
+  const { handleSubmit, register, control } = useForm<CreateStudioForm>({
+    resolver: zodResolver(schema),
+  });
+
+  // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+
+  //   const type = studioType?.value;
+
+  //   if (!type) {
+  //     return;
+  //   }
+
+  //   if (!StudioTypes.includes(type)) {
+  //     throw new Error("Invalid studio type");
+  //   }
+
+  //   const postData: NewStudio = {
+  //     name,
+  //     description,
+  //     imageUrl,
+  //     dateCreated,
+  //     type,
+  //   };
+
+  //   const { data } = await executePost({ data: postData });
+
+  //   const { id } = data;
+
+  //   navigate(`../../studio/${id}`);
+
+  //   if (error) {
+  //     throw new Error(error.message);
+  //   }
+  // };
+
   return (
-    <div className="form-control w-full max-w-xs">
-      <form onSubmit={handleSubmit}>
-        <div className="label">
-          <span className="label-text">Studio type:</span>
-        </div>
-        <Select
-          className="text-black"
-          options={StudioOptions}
-          placeholder="Select studio type"
-          onChange={(item) => setStudioType(item)}
-          defaultValue={studioType}
-          isClearable={!!studioType}
-        />
-        <div className="label">
-          <span className="label-text">Name:</span>
-        </div>
-        <input
-          type="text"
-          placeholder="Name"
-          className="input input-bordered w-full max-w-xs"
-          value={name}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <label className="form-control">
-          <div className="label">
-            <span className="label-text">Description</span>
+    <div className="flex gap-48">
+      <div className="form-control w-full max-w-xs">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-2">
+            <span className="label-text">Studio type:</span>
+            <Controller
+              defaultValue={StudioTypes[0]}
+              control={control}
+              name="type"
+              render={({ field }) => {
+                return (
+                  <Select onValueChange={field.onChange} {...field}>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select studio type" />
+                    </SelectTrigger>
+                    <SelectContent className="bg-base-100">
+                      <SelectGroup>
+                        <SelectLabel>Studio types</SelectLabel>
+                        {StudioTypes.map((item) => (
+                          <SelectItem key={item} value={item}>
+                            {item}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                );
+              }}
+            />
+            <div className="label">
+              <span className="label-text">Name:</span>
+            </div>
+            <input
+              {...register("name")}
+              type="text"
+              placeholder="Name"
+              className="input input-bordered input-sm w-full max-w-xs"
+            />
+
+            <div className="label">
+              <span className="label-text">Description:</span>
+            </div>
+            <textarea
+              {...register("description")}
+              placeholder="Enter description..."
+              className="textarea textarea-bordered"
+            />
+
+            <div className="label">
+              <span className="label-text">Image URL:</span>
+            </div>
+            <input
+              {...register("imageUrl")}
+              type="text"
+              placeholder="Image URL"
+              className="input input-bordered input-sm w-full max-w-xs"
+            />
+
+            <div className="label">
+              <span className="label-text">Date created:</span>
+            </div>
+            <input
+              {...register("dateCreated")}
+              type="date"
+              className="input input-bordered input-sm w-full max-w-xs"
+            />
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
           </div>
-          <textarea
-            className="textarea textarea-bordered h-24"
-            placeholder="Description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          ></textarea>
-        </label>
-        <div className="label">
-          <span className="label-text">Image URL:</span>
-        </div>
-        <input
-          type="text"
-          placeholder="URL"
-          className="input input-bordered w-full max-w-xs"
-          value={imageUrl}
-          onChange={(e) => setImageUrl(e.target.value)}
-        />
-        <div className="label">
-          <span className="label-text">Date Founded:</span>
-        </div>
-        <input
-          type="text"
-          placeholder="Date (YYYY-MM-DD)"
-          className="input input-bordered w-full max-w-xs"
-          value={dateCreated}
-          onChange={(e) => setDateCreated(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="btn btn-active btn-ghost mt-4 w-80"
-          disabled={loading}
-        >
-          {loading ? (
-            <span className="loading loading-spinner"></span>
-          ) : (
-            "Submit"
-          )}
-        </button>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
