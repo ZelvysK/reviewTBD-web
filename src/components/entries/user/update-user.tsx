77bbcbf10b790a334a91b2cf3c778ec9cc479c99
Karolis Zelvys <koralbish@gmail.com>
2024-03-13 19:47:00 +0200

    Testing `Tabs` UI element

--- src/components/entries/user/update-user.tsx
+++ src/components/entries/user/update-user.tsx
@@ -18,6 +18,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "../../form/select";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../../../utils/tabs";
 
 const schema = z.object({
   userName: z.string().min(3),
@@ -72,7 +73,12 @@ export const UpdateUser = () => {
   return (
     <div className="flex gap-48">
       <div className="form-control w-full max-w-xs">
-        <UpdateUserForm user={data} submitData={onSubmit} />
+        {data.role === "User" && (
+          <UpdateUserForm user={data} submitData={onSubmit} />
+        )}
+        {data.role === "Admin" && (
+          <UpdateUserFormTabs user={data} submitData={onSubmit} />
+        )}
       </div>
     </div>
   );
@@ -184,5 +190,124 @@ const UpdateUserForm = ({ user, submitData: submit }: Props) => {
         </Link>
       </div>
     </form>
+  );
+};
+
+const UpdateUserFormTabs = ({ user, submitData: submit }: Props) => {
+  const { formState, handleSubmit, register, control } =
+    useForm<UpdateUserFormData>({
+      resolver: zodResolver(schema),
+      values: user,
+    });
+
+  return (
+    <>
+      <Tabs defaultValue="user" className="w-[400px]">
+        <TabsList className="border border-white">
+          <TabsTrigger value="user">User Update</TabsTrigger>
+          <TabsTrigger value="admin">Admin Update</TabsTrigger>
+        </TabsList>
+        <TabsContent value="user">
+          <form onSubmit={handleSubmit(submit)}>
+            <div className="flex flex-col gap-2">
+              <div className="label">
+                <span className="label-text">Username:</span>
+              </div>
+              <input
+                {...register("userName")}
+                type="text"
+                placeholder="Username..."
+                className="input input-bordered input-sm w-full max-w-xs"
+              />
+              <ErrorMessage
+                name="userName"
+                errors={formState.errors}
+                render={({ message }) => (
+                  <p className="text-error">{message}</p>
+                )}
+              />
+
+              <div className="label">
+                <span className="label-text">Email:</span>
+              </div>
+              <input
+                type="email"
+                {...register("email")}
+                placeholder="Email..."
+                className="input input-bordered input-sm w-full max-w-xs"
+              />
+              <ErrorMessage
+                name="email"
+                errors={formState.errors}
+                render={({ message }) => (
+                  <p className="text-error">{message}</p>
+                )}
+              />
+
+              <div className="label">
+                <span className="label-text">Phone number:</span>
+              </div>
+              <input
+                {...register("phoneNumber")}
+                type="text"
+                placeholder="Phone number..."
+                className="input input-bordered input-sm w-full max-w-xs"
+              />
+              <ErrorMessage
+                name="phoneNumber"
+                errors={formState.errors}
+                render={({ message }) => (
+                  <p className="text-error">{message}</p>
+                )}
+              />
+            </div>
+          </form>
+        </TabsContent>
+
+        <TabsContent value="admin">
+          <div className="label">
+            <span className="label-text">Role:</span>
+          </div>
+          <Controller
+            defaultValue={user.role}
+            control={control}
+            name="role"
+            render={({ field }) => {
+              return (
+                <Select onValueChange={field.onChange} {...field}>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select user role" />
+                  </SelectTrigger>
+                  <SelectContent className="bg-base-100">
+                    <SelectGroup>
+                      <SelectLabel>User roles</SelectLabel>
+                      {RoleTypes.map((item) => (
+                        <SelectItem key={item} value={item}>
+                          {item}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              );
+            }}
+          />
+          <ErrorMessage
+            name="role"
+            errors={formState.errors}
+            render={({ message }) => <p className="text-error">{message}</p>}
+          />
+        </TabsContent>
+      </Tabs>
+      <div className="flex flex-col gap-2 mt-3">
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+
+        <Link to={`/user/changePassword/${user.id}`} className="btn btn-info">
+          Change password
+        </Link>
+      </div>
+    </>
   );
 };
